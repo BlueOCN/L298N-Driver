11c5ba36fcddcd6a0fd71041acf09f815f3cea54
BlueOCN <robgonzalezmu@gmail.com>
2025-11-08 18:51:20 -0600

    Update - Removed library.json files at subdirectories.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,8 @@
 #include <Arduino.h>
-#include "L298N.h"
-#include "pins/Pins.h"
-#include "pins/builder/L298N_Pins_Builder.h"
+#include "L298N_Driver.h"
 
-Pins pins = L298N_Pins_Builder().setIn1Pin(1).setIn2Pin(2).build();
+Pins pins = L298N_Pins_Builder().setIn1Pin(2).setIn2Pin(3).build();
 L298N driver = L298N(Mode::SingleMotor, pins);
-
-L298N driver2 = L298N(Mode::DualMotorBasic, L298N_Pins_Builder().setIn1Pin(1).setIn2Pin(2).setIn3Pin(3).setIn4Pin(4).build());
 
 void setup() {
   BeginParams bp;
@@ -16,12 +12,21 @@ void setup() {
 void loop() {
 
   StartParams sp;
+  StopParams sop;
+
   sp.setMotor(MotorInstance::ALL);
   sp.setDirection(MotorDirection::CLOCKWISE);
   driver.start(sp);
   delay(2000);
 
-  StopParams sop;
+  sp.setMotor(MotorInstance::ALL);
+  driver.stop(sop);
+  delay(1000);
+
+  sp.setDirection(MotorDirection::COUNTER_CLOCKWISE);
+  driver.start(sp);
+  delay(2000);
+
   sp.setMotor(MotorInstance::ALL);
   driver.stop(sop);
   delay(1000);
